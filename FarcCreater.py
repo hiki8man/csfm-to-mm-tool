4ce6a31605910e2a065824dd7eb2a29449487d46
hiki8man <63759994+hiki8man@users.noreply.github.com>
2026-02-18 14:20:49 +0800

    优化sprite，按照jitter的方案实现jk图边缘平滑

--- FarcCreater.py
+++ FarcCreater.py
@@ -128,16 +128,35 @@ class Farc:
         farc.add_file_data(f"{export_name}.bin", spr_bin.to_buf())
         farc.write(str(export_path.joinpath(f"{export_name}.farc")), False, False)
 
+def fit_image(img:Image.Image, width:int, height:int, is_jk:bool=False) -> Image.Image:
+    img = ImageOps.fit(img, (width, height),Image.Resampling.LANCZOS)
+    # 需要扩展两像素出血
+    import numpy as np
+    img_array = np.array(img)
+    expand_data = np.pad(img_array, pad_width=((2,2),(2,2),(0,0)), mode='edge')
+    img = Image.fromarray(expand_data)
+    
+    if is_jk:
+        # 根据jitter的方案边缘需要设置透明
+        real_img = ImageOps.fit(img, (img.size[0] - 2, img.size[1] - 2))
+        img.putalpha(25)
+        img.paste(real_img, (1, 1))
+
+    return img
+
 def create_sel_texture_0(bg_path:Path, jk_path:Path|None = None) -> Image.Image:
+    import numpy as np
     img_data = Image.new("RGBA",(2048, 1024))
     if not jk_path:
         jk_path = bg_path
 
-    jk_img = ImageOps.fit(Image.open(jk_path), (500,500))
-    bg_img = ImageOps.fit(Image.open(bg_path), (1280,720))
+    bg_img = fit_image(Image.open(bg_path), 1280,720)
+    jk_img = fit_image(Image.open(jk_path), 500,500, True)
+    # jk_img = fit_image(Image.open(jk_path), 500,500)
     
-    img_data.paste(bg_img)
-    img_data.paste(jk_img, (1287,3 ,1787,503))
+    img_data.paste(bg_img, (0, 0))
+    img_data.paste(jk_img, (1287,3))
+    # img_data.paste(jk_img, (1284,0))
     
     return img_data.transpose(Transpose.FLIP_TOP_BOTTOM)
 
